c54e9350b9be24c1f5598365aaaf498bb90eec5f
zoe <zoeguerreroepet20@gmail.com>
2024-10-16 13:56:56 -0300

    corrigiendo cod. compu grnade

--- App.js
+++ App.js
@@ -1,31 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
 
-
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={{uri: './assets/fondo-inicio1'}} style={styles.backgroundImage}>
-        <Text style={styles.title}>VICINO</Text>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Iniciar sesión</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Registrarse</Text>
-        </TouchableOpacity>
-      </ImageBackground>
-      <ImageBackground source={{uri: './assets/fondo-inicio1'}} style={styles.backgroundImage}>
-        <Text style={styles.title}>VICINO</Text>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Iniciar sesión</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Registrarse</Text>
-        </TouchableOpacity>
-      </ImageBackground>
-    <View/>
-=======
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import Inicio from './screens/inicio1';
@@ -35,7 +8,6 @@ export default function App() {
     <View style={styles.container}>
       <Inicio />
       <Envio/>
->>>>>>> ee2d36e727030093a623dc194026977ebf5b129d
     </View>
   );
 }
