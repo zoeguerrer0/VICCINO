6af984a536a72ddde355a3f02fb624c3650b3a25
zoeGuerrero <zoeguerreroepet20@gmail.com>
2024-11-01 20:09:15 -0300

    solucionando error

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { View, StyleSheet,ScrollView, Text } from "react-native";
+import { View, StyleSheet,ScrollView, } from "react-native";
 import Usuarios from "./screens/usuario";
-import  { DataProvider } from "./src/componentes/DataContext";
-import { View, StyleSheet,ScrollView, Dimensions } from "react-native";
-//  terminar de configurar la dimension de las pantallas
 
 export default function App() {
   return (  
-    <DataProvider>
-      {/* bmnvm */}
+  
         <ScrollView>
           <View style={styles.container}>
             
@@ -16,7 +12,7 @@ export default function App() {
           
           </View>
         </ScrollView>
-    </DataProvider>
+
   )
 }
 
