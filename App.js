a02174a734e087b1dff85115edfde5f00f33c58f
zoeGuerrero <zoeguerreroepet20@gmail.com>
2024-11-03 21:10:07 -0300

    duplica de product.js así pruebo el carrito

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import 'react-native-gesture-handler';
 import MyStack from "./navegacion";
-
+import Product from "./screens/avanceCarrito";
 import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native-web";
 export default function App() {
   return (
-    <NavigationContainer>
+    <View>
+      <Product/>
+      <NavigationContainer>
       <MyStack />
     </NavigationContainer>
+    </View>
+    
   );
 }
