10630b0fe58150920ef1ac9668cb7cceb5b0f955
zoeGuerrero <zoeguerreroepet20@gmail.com>
2024-11-12 11:46:31 -0300

    solución error : gestor de props(?

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import 'react-native-gesture-handler';
 import { Nab} from './navigation/navegacion'; // Asegúrate de que esta importación sea correcta
-import { ModalComponent } from "./src/componentes/ComponenteModal";
 import { NavigationContainer } from "@react-navigation/native";
 
 export function App() {
